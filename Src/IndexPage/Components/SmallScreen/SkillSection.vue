ec8d093aebffe718be94eac90815bc684cdfbc45
Terra Vermillion <VermillionT@outlook.com>
2019-02-21 12:19:27 -0700

    Added skill section data

--- Src/IndexPage/Components/SmallScreen/SkillSection.vue
+++ Src/IndexPage/Components/SmallScreen/SkillSection.vue
@@ -3,35 +3,33 @@
         <h2 id="skillsHeader" class="sectionHeader">What technologies do I work with?</h2>
         <nav>
             <span id="left" class="menuArrow"></span>
-            <h3 id="skillsLabel">{{skillsLabel}}</h3>
+            <h3 id="skillsLabel">{{Label}}</h3>
             <span id="right" class="menuArrow"></span>
         </nav>
         <div id="skillsMessageContainer">
-            <p class="sectionText">{{skillParagraph1}}</p>
-            <p class="sectionText">{{skillParagraph2}}</p>
-            <img id="skillsImage" v-bind:src="skillsImageUrl" alt="">
+            <p class="sectionText">{{Paragraph1}}</p>
+            <p class="sectionText">{{Paragraph2}}</p>
+            <img id="skillsImage" v-bind:src="ImageUrl" alt="">
         </div>
     </section>
 </template>
 
 <script>
+    import skillsSectionData from "./../../Scripts/skillSectionData.js";
+
     export default {
         data() {
             return {
-                skillsLabel: "Javascript",
-                skillParagraph1: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                        Eos ad exercitationem dolorem adipisci? 
-                        Eaque corrupti repudiandae unde maxime ipsum architecto.`,
-                skillParagraph2: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Officiis fugit praesentium minima dolore et nobis 
-                        repudiandae a sunt cupiditate commodi excepturi ex 
-                        delectus, nulla quia veritatis omnis 
-                        voluptatum saepe quidem.`,
-                skillsImageUrl: "https://user-images.githubusercontent.com/14293805/36941746-0d65c6fa-1f63-11e8-8afa-ab2d28de86db.png"
+                Label: skillsSectionData[0].label,
+                Paragraph1: skillsSectionData[0].paragraph1,
+                Paragraph2: skillsSectionData[0].paragraph2,
+                ImageUrl: skillsSectionData[0].imageUrl
             }
         },
         method: {
-            
+            skillCycle() {
+
+            }
         }
     }
 </script>
@@ -46,6 +44,8 @@
         max-width: 100px;
         height: auto;
     }
+
+    
 
 </style>
 
---
+++ Src/IndexPage/Scripts/skillSectionData.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        label: "Javascript",
+        paragraph1: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Eos ad exercitationem dolorem adipisci? 
+                Eaque corrupti repudiandae unde maxime ipsum architecto.`,
+        paragraph2: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Officiis fugit praesentium minima dolore et nobis 
+                repudiandae a sunt cupiditate commodi excepturi ex 
+                delectus, nulla quia veritatis omnis 
+                voluptatum saepe quidem.`,
+        imageUrl: "https://user-images.githubusercontent.com/14293805/36941746-0d65c6fa-1f63-11e8-8afa-ab2d28de86db.png"
+    },
+    {
+        label: "CSS",
+        paragraph1: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Eos ad exercitationem dolorem adipisci? 
+                Eaque corrupti repudiandae unde maxime ipsum architecto.`,
+        paragraph2: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Officiis fugit praesentium minima dolore et nobis 
+                repudiandae a sunt cupiditate commodi excepturi ex 
+                delectus, nulla quia veritatis omnis 
+                voluptatum saepe quidem.`,
+        imageUrl: "https://fuzati.com/wp-content/uploads/2016/12/CSS-Logo.png"
+    }
+]
